cbd24b94681bb505e3b1dfd3554c29ba8dc18c94
Marek <Marek@DESKTOP-4PKM5S4>
2021-10-31 11:04:32 +0100

    Add project files.

---
+++ GrammarTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrammarTool.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
